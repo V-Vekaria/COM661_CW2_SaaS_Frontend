a222730cca9772b53aa31568314f984630e22879
Vishnu Vekaria <vekariyavishnu63@gmail.com>
2026-04-22 09:00:22 +0100

    feat: add routes for all components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
+import { UsersComponent } from './components/users/users';
+import { UserComponent } from './components/user/user';
+import { ActivityLogsComponent } from './components/activity-logs/activity-logs';
+import { AnomalyFlagsComponent } from './components/anomaly-flags/anomaly-flags';
+import { AnalyticsComponent } from './components/analytics/analytics';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
+  { path: 'activity-logs', component: ActivityLogsComponent },
+  { path: 'anomaly-flags', component: AnomalyFlagsComponent },
+  { path: 'analytics', component: AnalyticsComponent }
+];
